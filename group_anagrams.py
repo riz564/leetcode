01d7b2d0d0551969319dd0e78bede26772949d19
Md. Rizwan Rabbani <33202688+riz564@users.noreply.github.com>
2025-06-14 11:13:08 +0530

    Create group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups=defaultdict(list)
+        for st in strs:
+            count=[0]*26
+            for c in st:
+                count[ord(c)-ord('a')]+=1
+            groups[tuple(count)].append(st)
+
+        return [v for k,v in groups.items()]
